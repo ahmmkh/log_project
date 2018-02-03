6f941edff71d0dd1f9e928421f9a20fa703f9b22
AHmed Khatab <b-b@dr.com>
2018-02-04 00:48:19 +0200

    added files

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-log_project
+# Summery
+
+this projec is a reporting tool for a database for  news website and report it in a plain text 
+
+## installation
+
+### Requirments
+
+- python
+- vagrant
+- virtualbox
+- psycopg2 `pip install psycopg2`
+- [fullstack-nanodegree-vm](https://github.com/udacity/fullstack-nanodegree-vm)
+
+### Setup
+
+after you cloned the repo `git clone https://github.com/udacity/fullstack-nanodegree-vm fullstack`
+you should `cd fullstack/vagrant` then `vagrant up` this will download an image of linux distro with all file included you will take the content of ths repo and add it to that folder and then run the code using `python log.py` this will generate report.txt file with reported data.
---
+++ log.py
@@ -0,0 +1,93 @@
+import psycopg2
+postfix = ["views", "views", "%"]
+postfix_msg = ["the most popular three articles of all time",
+               "the most popular article authors of all time",
+               "On which days did more than 1%% of requests lead to errors"]
+QUERY_1 = ("SELECT "
+           "articles.title, "
+           "COUNT(log.path) "
+           "FROM "
+           "log "
+           "INNER JOIN articles ON CONCAT('/article/', articles.slug) = log.path "
+           "GROUP BY "
+           "articles.title "
+           "ORDER BY "
+           "COUNT(log.path) DESC "
+           "LIMIT "
+           "3")
+QUERY_2 = ("SELECT "
+           "authors.name, "
+           "COUNT(log.path) "
+           "FROM "
+           "log "
+           "INNER JOIN articles ON CONCAT('/article/', articles.slug) = log.path "
+           "INNER JOIN authors ON articles.author = authors.id "
+           "GROUP BY "
+           "authors.name "
+           "ORDER BY "
+           "COUNT(log.path) DESC")
+QUERY_3 = ("SELECT "
+           "normal.day, "
+           "ROUND(((err.errors + 0.0) / (normal.pass_code+0.0))*100 , 2) as perc "
+           "FROM "
+           "( "
+           "SELECT "
+           "DATE(time) as day, "
+           "COUNT(DATE(time)) as errors "
+           "FROM "
+           "log "
+           "WHERE "
+           "status = '404 NOT FOUND' "
+           "GROUP BY "
+           "DATE(time) "
+           "ORDER BY "
+           "DATE(time) DESC "
+           ") err "
+           "INNER JOIN ( "
+           "SELECT "
+           "DATE(time) as day, "
+           "COUNT(DATE(time)) as pass_code "
+           "FROM "
+           "log "
+           "WHERE "
+           "status = '200 OK' "
+           "GROUP BY "
+           " DATE(time) "
+           "ORDER BY "
+           "DATE(time) DESC "
+           ") normal ON err.day = normal.day "
+           "WHERE "
+           "ROUND(((err.errors + 0.0) / (normal.pass_code+0.0))*100 , 2) > 1")
+
+
+def connect():
+    """ Cnnect To the database  """
+    try:
+        db = psycopg2.connect("dbname=news")
+        cur = db.cursor()
+        return db, cur
+    except:
+        print("there is an error while connecting")
+
+
+def get_query(query):
+    """ get the query data in array of tuples """
+    db, cur = connect()
+    cur.execute(query)
+    data = cur.fetchall()
+    return data
+
+
+def print_data(data, pf, f):
+    """print data got from queries"""
+    f.write("--- "+postfix_msg[pf]+" ---\n\n")
+    for record in data:
+        f.write(str(record[0])+"____"+str(record[1])+" "+postfix[pf]+"\n")
+    f.write("\n ------------------ \n\n")
+
+
+if __name__ == '__main__':
+    queries = [QUERY_1, QUERY_2, QUERY_3]
+    with open("report.txt", "w+") as f:
+        for i in xrange(len(queries)):
+            print_data(get_query(queries[i]), i,f)
